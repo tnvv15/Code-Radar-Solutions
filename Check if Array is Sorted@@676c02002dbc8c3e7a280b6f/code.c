3d7313dc4bc2fff62f971e7addb2ef048261d1c2
Tanvi <tanvi15singla@gmail.com>
2025-04-01 10:04:46 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -9,7 +9,7 @@ int main(){
     }
     Bubblesort(arr,n);
 }
-    int Bubblesort(int arr[],int n){
+    void Bubblesort(int arr[],int n){
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]<arr[j+1]){
