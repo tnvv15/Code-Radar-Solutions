9ddb513c642b9f41be1cc010d88523615ea29486
Tanvi <tanvi15singla@gmail.com>
2025-04-10 13:14:16 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,9 +1,10 @@
 // Your code here...
 #include <stdio.h>
+#include <stdlib.h>
 int Bubblesort(int arr[], int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
+            if(abs(arr[j])>abs(arr[j+1])){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
