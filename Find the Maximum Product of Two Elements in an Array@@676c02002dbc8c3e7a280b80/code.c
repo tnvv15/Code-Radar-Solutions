70e72a647b3ac5088733eaaada062afd20aa88b1
Tanvi <tanvi15singla@gmail.com>
2025-04-10 13:17:24 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,23 +1,58 @@
-// Your code here...
+// // Your code here...
+// #include <stdio.h>
+// #include <stdlib.h>
+// int Bubblesort(int arr[], int n){
+//     for(int i=0;i<n-1;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             if(abs(arr[j])>abs(arr[j+1])){
+//                 int temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=temp;
+//             }
+//         }
+//     }
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//     scanf("%d ",&arr[i]);}
+//     Bubblesort(arr,n);
+//     printf("%d",arr[n-2]*arr[n-1]);
+// }
 #include <stdio.h>
-#include <stdlib.h>
-int Bubblesort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(abs(arr[j])>abs(arr[j+1])){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    int max1 = INT_MIN, max2 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        } else if(arr[i] > max2) {
+            max2 = arr[i];
+        }
+
+        if(arr[i] < min1) {
+            min2 = min1;
+            min1 = arr[i];
+        } else if(arr[i] < min2) {
+            min2 = arr[i];
         }
     }
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-    scanf("%d ",&arr[i]);}
-    Bubblesort(arr,n);
-    printf("%d",arr[n-2]*arr[n-1]);
+
+    int product1 = max1 * max2;
+    int product2 = min1 * min2;
+
+    printf("%d\n", (product1 > product2) ? product1 : product2);
+    return 0;
 }
